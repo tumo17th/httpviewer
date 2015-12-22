2962f92c3e9b9cfcfc039858808e7af8a4fbd45d
tumo <tumo17th@gmail.com>
2015-12-23 02:41:28 +0900

    refs #1 Add null check to print body logic

--- httpviewer/src/main/java/com/shibatats/httpviewer/app/impl/HttpViewerImpl.java
+++ httpviewer/src/main/java/com/shibatats/httpviewer/app/impl/HttpViewerImpl.java
@@ -25,86 +25,93 @@ import com.shibatats.httpviewer.printer.Printer;
 @Component
 public class HttpViewerImpl implements HttpViewer {
 
-	@Autowired
-	private ConfigGetter config;
+    @Autowired
+    private ConfigGetter config;
 
-	@Autowired
-	private Printer printer;
+    @Autowired
+    private Printer printer;
 
-	private static final String LS = System.getProperty("line.separator");
+    private static final String LS = System.getProperty("line.separator");
 
-	// Public Method ///////////////////////////////////////////////////////////////////////////////////////////////////
-	@Override
-	public void doService() {
-		HttpURLConnection urlConn = null;
-		try {
-			urlConn = getHttpURLConnection();
-			urlConn.connect();
+    // Public Method ///////////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void doService() {
+        HttpURLConnection urlConn = null;
+        try {
+            urlConn = getHttpURLConnection();
+            urlConn.connect();
 
-			String headerStr = getHeaderStr(urlConn);
-			String bodyStr = printBody(urlConn);
-			String content = headerStr + bodyStr;
+            String headerStr = getHeaderStr(urlConn);
+            String bodyStr = printBody(urlConn);
+            String content = headerStr + bodyStr;
 
-			printer.printToConsole(content);
-			if (config.isPrintToFile()) {
-				printer.printToFile(content);
-			}
+            printer.printToConsole(content);
+            if (config.isPrintToFile()) {
+                printer.printToFile(content);
+            }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (urlConn != null) {
-				urlConn.disconnect();
-			}
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConn != null) {
+                urlConn.disconnect();
+            }
+        }
+    }
 
-	// Private Method //////////////////////////////////////////////////////////////////////////////////////////////////
-	private HttpURLConnection getHttpURLConnection() throws MalformedURLException, IOException {
-		String targetURL = config.getTargetURL();
-		String proxyHost = config.getProxyHost();
-		String proxyPort = config.getProxyPort();
+    // Private Method //////////////////////////////////////////////////////////////////////////////////////////////////
+    private HttpURLConnection getHttpURLConnection() throws MalformedURLException, IOException {
+        String targetURL = config.getTargetURL();
+        String proxyHost = config.getProxyHost();
+        String proxyPort = config.getProxyPort();
 
-		URL url = new URL(targetURL);
-		if (!StringUtils.isEmpty(proxyHost) && !StringUtils.isEmpty(proxyPort) && StringUtils.isNumeric(proxyPort)) {
-			Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort)));
-			return (HttpURLConnection)url.openConnection(proxy);
-		}
-		return (HttpURLConnection)url.openConnection();
-	}
+        URL url = new URL(targetURL);
+        if (!StringUtils.isEmpty(proxyHost) && !StringUtils.isEmpty(proxyPort) && StringUtils.isNumeric(proxyPort)) {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort)));
+            return (HttpURLConnection)url.openConnection(proxy);
+        }
+        return (HttpURLConnection)url.openConnection();
+    }
 
-	private String getHeaderStr(HttpURLConnection urlConn) {
-		StringBuilder sb = new StringBuilder();
-		sb.append(LS + "----------- Header -----------" + LS);
-		Map<String, List<String>> headerList = urlConn.getHeaderFields();
-		for (Entry<String, List<String>> entry : headerList.entrySet()) {
-			String key = entry.getKey();
-			List<String> valList = entry.getValue();
-			sb.append(key + " : " + valList + LS);
-		}
-		return sb.toString();
-	}
+    private String getHeaderStr(HttpURLConnection urlConn) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(LS + "----------- Header -----------" + LS);
+        Map<String, List<String>> headerList = urlConn.getHeaderFields();
+        for (Entry<String, List<String>> entry : headerList.entrySet()) {
+            String key = entry.getKey();
+            List<String> valList = entry.getValue();
+            sb.append(key + " : " + valList + LS);
+        }
+        return sb.toString();
+    }
 
-	private String printBody(HttpURLConnection urlConn) throws IOException, UnsupportedEncodingException {
-		StringBuilder sb = new StringBuilder();
-		sb.append(LS + "----------- Body -----------" + LS);
+    private String printBody(HttpURLConnection urlConn) throws IOException, UnsupportedEncodingException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(LS + "----------- Body -----------" + LS);
 
-		InputStream resStream = getResultStream(urlConn);
-		String encoding = config.getEncoding();
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(resStream, encoding))) {
-			String line;
-			while ((line = br.readLine()) != null) {
-				sb.append(line + LS);
-			}
-		}
-		return sb.toString();
-	}
+        InputStream resStream = getResultStream(urlConn);
+        if (resStream == null)
+            return "This response has no HttpBody.";
 
-	private InputStream getResultStream(HttpURLConnection urlConn) throws IOException {
-		if (urlConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-			return urlConn.getInputStream();
-		}
-		return urlConn.getErrorStream();
-	}
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        String encoding = config.getEncoding();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(resStream, encoding))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line + LS);
+            }
+        }
+        return sb.toString();
+    }
+
+    private InputStream getResultStream(HttpURLConnection urlConn) throws IOException {
+        switch (urlConn.getResponseCode()) {
+        case HttpURLConnection.HTTP_OK:
+            return urlConn.getInputStream();
+        case HttpURLConnection.HTTP_NOT_FOUND:
+            return null;
+        }
+
+        return urlConn.getErrorStream();
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
